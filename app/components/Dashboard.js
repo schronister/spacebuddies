e56b22fffb32018a992228091105b8078eaede38
Steve Chronister <Steve Chronister>
2017-02-01 18:42:11 -0600

    adjust function

--- app/components/Dashboard.js
+++ app/components/Dashboard.js
@@ -49,18 +49,20 @@ var Dashboard = React.createClass({
     },
 
     fbAuth: function(){
+        var info;
         FB.login(function(response) {
         if (response.authResponse) {
          console.log('Welcome!  Fetching your information.... ');
          FB.api('/me', function(response) {
            console.log('Good to see you, ' + response.name + '.');
-           console.log("this", this);
+          info = response.name
            callSetUser(response.name);
          });
         } else {
          console.log('User cancelled login or did not fully authorize.');
         }
     });
+        this.props.setUser(info);
     },
 
     callSetUser: function(name){
